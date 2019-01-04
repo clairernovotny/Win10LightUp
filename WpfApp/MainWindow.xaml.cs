4294a2a8abb0409ecf205e570fa990c81066423c
Oren Novotny <oren@novotny.org>
2019-01-03 19:44:19 -0500

    Initial commit

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Notifications.Wpf;
+using OSVersionHelper;
+using Windows.UI.Notifications;
 
 namespace WpfApp
 {
@@ -25,9 +18,49 @@ namespace WpfApp
             InitializeComponent();
         }
 
-        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        private void ClickMe_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            if(WindowsVersionHelper.HasPackageIdentity)
+            {
+                ShowToastUsingWindowsRuntime();
+            }
+            else
+            {
+                ShowToastDownlevel();
+            }
+        }
+
+        private void ShowToastUsingWindowsRuntime()
+        {
+            // Get a toast XML template
+            var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText03);
+
+            // Fill in the text elements
+            var stringElements = toastXml.GetElementsByTagName("text");
+            for (var i = 0; i < stringElements.Length; i++)
+            {
+                stringElements[i].AppendChild(toastXml.CreateTextNode("Line " + i));
+            }
+
+            // Specify the absolute path to an image
+            var imagePath = "file:///" +  Path.Combine(Path.GetDirectoryName(typeof(MainWindow).Assembly.Location), "toastImageAndText.png");
+            var imageElements = toastXml.GetElementsByTagName("image");
+            imageElements[0].Attributes.GetNamedItem("src").NodeValue = imagePath;
+            
+            var toast = new ToastNotification(toastXml);
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        private void ShowToastDownlevel()
+        {
+            var notificationManager = new NotificationManager();
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "The Title",
+                Message = "The message",
+                Type = NotificationType.Information
+            });
         }
     }
 }
